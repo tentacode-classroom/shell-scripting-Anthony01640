f74ece3d47614d20273620c03b24a148e6f5be1d
Anthony01640 <anthodu01640@hotmail.fr>
2018-12-18 15:34:51 +0100

    suite

--- menage.sh
+++ menage.sh
@@ -1,13 +1,30 @@
 echo "Nettoyage"
 
 du -a bazar/
-
+rm -rf bazar_clean
 mkdir bazar_clean
 mkdir bazar_clean/video
 mkdir bazar_clean/images
 mkdir bazar_clean/divers
 
-find bazar/01/ -name "*.jpg" -exec cp {} bazar_clean/images \;
-find bazar/01/ -name "*.mp4" -exec cp {} bazar_clean/video \;
-find bazar/01/ -name "*.txt" -exec cp {} bazar_clean/divers \;
+FILES=`find bazar/ -type f `
 
+for FILE in $FILES
+do 
+
+	file $FILE | grep 'image data' > /dev/null
+	IS_IMAGE=$?
+
+	file $FILE | grep 'ISO media' > /dev/null
+	IS_VIDEO=$?
+
+	if [ $IS_IMAGE == 0 ]
+	then
+		cp $FILE bazar_clean/images
+	elif [ $IS_VIDEO == 0  ]
+	then
+		cp $FILE bazar_clean/video
+	else
+		cp $FILE bazar_clean/divers
+	fi
+done
